35fbd66d45cc350a9ab6f8b983c39f917ab1bb92
Bian-1cheng <1194832004@qq.com>
2023-12-11 20:17:53 +0800

    实现刷新token拦截

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,12 +1,19 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefershTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -18,6 +25,7 @@ public class MvcConfig implements WebMvcConfigurer {
                 "/blog/hot",
                 "/user/code",
                 "/user/login"
-        );
+        ).order(1);
+        registry.addInterceptor(new RefershTokenInterceptor(redisTemplate)).addPathPatterns("/**").order(0);
     }
 }
