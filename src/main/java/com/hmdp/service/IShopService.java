2d23992d97f52e11fc9b995ed7a6e216219e59f5
Bian-1cheng <1194832004@qq.com>
2023-12-13 17:05:01 +0800

    ”根据id获取商品“

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,16 +1,17 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
- * <p>
- *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * @description: 商品信息查询
+ * @param: id 商品id
+ * @return:
+ * @author BianCheng
+ * @date: 2023/12/13 15:23
  */
 public interface IShopService extends IService<Shop> {
 
+    Result getShopById(Long id);
 }
