f3a2c39a30c232bc3bbcd1f68742a825f65800e0
Bian-1cheng <1194832004@qq.com>
2023-12-14 19:44:13 +0800

    ”店铺类型获取“

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +25,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private IShopTypeService typeService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result shopList() {
+        String shopTypeJson = stringRedisTemplate.opsForValue().get("shopType:");
+        if(!StrUtil.isBlank(shopTypeJson)){
+            JSONArray shopType = JSONUtil.parseArray(shopTypeJson);
+            return Result.ok(shopType);
+        }
+        List<ShopType> shopList = typeService.query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set("shopType:",JSONUtil.toJsonStr(shopList));
+        return Result.ok(shopList);
+    }
 }
